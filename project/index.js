12de3a4a380fdd2600b8ea8c3a424ead195ccc5a
Josh <grafton.josh@gmail.com>
2018-08-21 15:18:27 -0600

    Jquery
    Added jquery button to hide content

--- project/index.js
+++ project/index.js
@@ -1,13 +1,6 @@
-//Two arrays (two parameters). Intertwine them
-//Array1: ["D", "O", "G"], Array2: ["C", "A", "T"]
-//Output: ["D", "C", "O", "A", "G", "T"]
 
-const array = (arrayOne, arrayTwo) => {
-  let newArray = []
-  //let combinedArray = arrayOne.concat(arrayTwo)
-  for (let i = 0; i < arrayOne.length || i < arrayTwo.length; i++) {
-    newArray.push(arrayOne[i], arrayTwo[i])
-  }
-  return newArray
-}
-array(["D", "O", "G"], ["C", "A", "T"])
+  $(document).ready(function() {
+    $("button").click(function() {
+      $("p").hide();
+    });
+  });
